7736766e95833fbcd25d72fb5d331a7b4999db83
DanilSPB <sherlokDP@gmail.com>
2023-02-14 12:54:57 +0300

    Added interfaces for DB, DbContext and Initializer

--- Notes.Backend/Notes.Persistence/EntityTypeConfiguration/NoteConfiguration.cs
+++ Notes.Backend/Notes.Persistence/EntityTypeConfiguration/NoteConfiguration.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Notes.Persistence.EntityTypeConfiguration
 {
-	internal class NoteConfiguration
+	public class NoteConfiguration
 	{
 	}
 }
