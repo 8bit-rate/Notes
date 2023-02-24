b82e7e5d90b4309d616cb3e2d65760f028971f02
DanilSPB <sherlokDP@gmail.com>
2023-02-24 17:54:28 +0300

    Added unit tests

---
+++ Notes.Backend/Notes.Tests/Common/NotesContextFactory.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Domain;
+using Notes.Persistence;
+
+namespace Notes.Tests.Common
+{
+	public class NotesContextFactory
+	{
+		public static Guid UserAId = Guid.NewGuid();
+		public static Guid UserBId = Guid.NewGuid();
+
+		public static Guid NoteIdForDelete = Guid.NewGuid();
+		public static Guid NoteIdForUpdate = Guid.NewGuid();
+
+		public static NotesDbContext Create()
+		{
+			var options = new DbContextOptionsBuilder<NotesDbContext>()
+				.UseInMemoryDatabase(Guid.NewGuid().ToString())
+				.Options;
+
+			var context = new NotesDbContext(options);
+			context.Database.EnsureCreated();
+			context.Notes.AddRange
+				(
+					new Note
+					{
+						CreationDate = DateTime.Today,
+						Details = "Test details 1",
+						EditDate = null,
+						Id = Guid.Parse("C771208D-7F57-45C3-B836-45EB1E64E183"),
+						Title = "Test title 1",
+						UserId = UserAId
+					},
+					new Note
+					{
+						CreationDate = DateTime.Today,
+						Details = "Test details 2",
+						EditDate = null,
+						Id = Guid.Parse("E73ADD77-C368-4276-A646-2598D1D009F2"),
+						Title = "Test title 2",
+						UserId = UserBId
+					},
+					new Note
+					{
+						CreationDate = DateTime.Today,
+						Details = "Test details 3",
+						EditDate = null,
+						Id = NoteIdForDelete,
+						Title = "Test title 3",
+						UserId = UserAId
+					},
+					new Note
+					{
+						CreationDate = DateTime.Today,
+						Details = "Test details 4",
+						EditDate = null,
+						Id = NoteIdForUpdate,
+						Title = "Test title 4",
+						UserId = UserBId
+					}
+				);
+
+			context.SaveChanges();
+			return context;
+		}
+
+		public static void Destroy(NotesDbContext context)
+		{
+			context.Database.EnsureDeleted();
+			context.Dispose();
+		}
+	}
+}
---
+++ Notes.Backend/Notes.Tests/Notes/Queries/GetNoteDetailsQueryHandlerTests.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Notes.Application.Notes.Queries.GetNoteDetails;
+using Notes.Persistence;
+using Notes.Tests.Common;
+using Shouldly;
+
+namespace Notes.Tests.Notes.Queries
+{
+	[Collection("QueryCollection")]
+	public class GetNoteDetailsQueryHandlerTests
+	{
+		private readonly NotesDbContext Context;
+		private readonly IMapper Mapper;
+
+		public GetNoteDetailsQueryHandlerTests(QueryTestFixture fixture) =>
+			(Context, Mapper) = (fixture.Context, fixture.Mapper);
+
+		[Fact]
+		public async Task GetNoteDetailsQueryHandler_Success()
+		{
+			// Arrange
+			var handler = new GetNoteDetailsQueryHandler(Context, Mapper);
+
+			// Act
+			var result = await handler.Handle(
+				new GetNoteDetailsQuery
+				{
+					UserId = NotesContextFactory.UserBId,
+					Id = Guid.Parse("E73ADD77-C368-4276-A646-2598D1D009F2")
+				},
+				CancellationToken.None);
+
+			// Assert
+			result.ShouldBeOfType<NoteDetailsVm>();
+			result.Title.ShouldBe("Test title 2");
+			result.CreationDate.ShouldBe(DateTime.Today);
+		}
+	}
+}
