f22f7ef214e1ba8a23a5c55ee5edb45c043d187d
DanilSPB <sherlokDP@gmail.com>
2023-02-25 17:43:21 +0300

    Added versioning

---
+++ Notes.Backend/Notes.WebApi/ConfigureSwaggerOption.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Reflection;
+
+namespace Notes.WebApi
+{
+	public class ConfigureSwaggerOption : IConfigureOptions<SwaggerGenOptions>
+	{
+		private readonly IApiVersionDescriptionProvider _provider;
+
+		public ConfigureSwaggerOption(IApiVersionDescriptionProvider provider) => _provider = provider;
+
+		public void Configure(SwaggerGenOptions options)
+		{
+			foreach (var description in _provider.ApiVersionDescriptions)
+			{
+				var apiVersion = description.ApiVersion.ToString();
+				options.SwaggerDoc(description.GroupName,
+					new OpenApiInfo
+					{
+						// TODO: Добавить что-то в информацию
+						Version = apiVersion,
+						Title = $"Notes API {apiVersion}",
+						Description =
+							"A simple ASP NET Core Web API application.",
+						Contact = new OpenApiContact
+						{
+							Name = "Danil",
+							Email = "sherlokDP@gmail.com"
+						},
+						License = new OpenApiLicense
+						{
+							Name = "License",
+
+						}
+					});
+				options.AddSecurityDefinition($"AuthToken {apiVersion}",
+					new OpenApiSecurityScheme
+					{
+						In = ParameterLocation.Header,
+						Type = SecuritySchemeType.Http,
+						BearerFormat = "JWT",
+						Scheme = "bearer",
+						Name = "Authorization",
+						Description = "Authorization token"
+					});
+				options.AddSecurityRequirement(new OpenApiSecurityRequirement
+				{
+					{
+						new OpenApiSecurityScheme
+						{
+							Reference = new OpenApiReference
+							{
+								Type = ReferenceType.SecurityScheme,
+								Id = $"AuthToken {apiVersion}",
+							}
+						},
+						new string[] { }
+					}
+				});
+				options.CustomOperationIds(apiDescription =>
+					apiDescription.TryGetMethodInfo(out MethodInfo methodInfo)
+						? methodInfo.Name
+						: null);
+			}
+		}
+	}
+}
--- Notes.Backend/Notes.WebApi/Startup.cs
+++ Notes.Backend/Notes.WebApi/Startup.cs
@@ -5,6 +5,10 @@ using Notes.Persistence;
 using Notes.WebApi.Middleware;
 using System.Reflection;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Builder;
 
 namespace Notes.WebApi
 {
@@ -47,15 +51,21 @@ namespace Notes.WebApi
 					options.RequireHttpsMetadata = false;
 				});
 
+			services.AddVersionedApiExplorer(options => options.GroupNameFormat = "'v'VVV");
+			services.AddTransient<IConfigureOptions<SwaggerGenOptions>,
+					ConfigureSwaggerOption>();
 			services.AddSwaggerGen(config =>
 			{
 				var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
 				var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
 				config.IncludeXmlComments(xmlPath);
 			});
+
+			services.AddApiVersioning();
 		}
 
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env,
+			IApiVersionDescriptionProvider provider)
 		{
 			if (env.IsDevelopment())
 			{
@@ -65,8 +75,14 @@ namespace Notes.WebApi
 			app.UseSwagger();
 			app.UseSwaggerUI(config =>
 			{
-				config.RoutePrefix = string.Empty;
-				config.SwaggerEndpoint("swagger/v1/swagger.json", "Notes API");
+				foreach (var description in provider.ApiVersionDescriptions)
+				{
+					config.SwaggerEndpoint(
+						$"/swagger/{description.GroupName}/swagger.json",
+						description.GroupName.ToUpperInvariant());
+
+					config.RoutePrefix = string.Empty;
+				}
 			});
 			app.UseCustomExceptionHandler();
 			app.UseRouting();
@@ -74,6 +90,7 @@ namespace Notes.WebApi
 			app.UseCors("AllowAll");
 			app.UseAuthentication();
 			app.UseAuthorization();
+			app.UseApiVersioning();
 
 			app.UseEndpoints(endpoints =>
 			{
